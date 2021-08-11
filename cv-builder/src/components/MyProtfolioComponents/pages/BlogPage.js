29546d3dd82c1a0985424ba0208ef51c0229d909
mohamad <msiysinyuy@gmail.com>
2021-08-11 23:00:29 +0100

    the blog page complete

--- cv-builder/src/components/MyProtfolioComponents/pages/BlogPage.js
+++ cv-builder/src/components/MyProtfolioComponents/pages/BlogPage.js
@@ -1,8 +1,32 @@
 import '../../../styles/BlogPage.css'
 import '../../../styles/MainLayout.css'
+
+import blogs from "../data/blogs"
+import Title from '../../Title'
+
 function BlogPage(){
   return(
-    <div className="all">this is the blog page</div>
+    <div className="all ">
+    <div className=" blogpage__container ">
+    <Title title={'Blogs'} span={'Blogs'} />
+    <div className="innerLayout blog">
+    {blogs.map((blog)=>{
+      return <div key={blog.id} className="blog__item">
+        <div className="blog__image">
+          <img src={blog.image} alt=""/>
+        </div>
+        <div className="title">
+          <a href={blog.link}>
+            {blog.title}
+          </a>
+        </div>
+      </div>
+    })}
+    </div>
+
+
+    </div>
+    </div>
   )
 
 }
