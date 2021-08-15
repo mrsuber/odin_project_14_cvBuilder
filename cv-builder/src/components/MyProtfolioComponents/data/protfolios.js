81fe4204511aad34fa03fc1f1f179d1517966f5c
mohamad <msiysinyuy@gmail.com>
2021-08-15 22:24:10 +0100

    the porfolio updated to date

--- cv-builder/src/components/MyProtfolioComponents/data/protfolios.js
+++ cv-builder/src/components/MyProtfolioComponents/data/protfolios.js
@@ -4,6 +4,13 @@ import img3 from '../../../images/portImages/img3.png'
 import img4 from '../../../images/portImages/img4.png'
 import img5 from '../../../images/portImages/img5.png'
 import img6 from '../../../images/portImages/img6.png'
+import img7 from '../../../images/portImages/img7.png'
+import img8 from '../../../images/portImages/img8.png'
+import img9 from '../../../images/portImages/img9.png'
+import img10 from '../../../images/portImages/img10.png'
+import img11 from '../../../images/portImages/img11.png'
+import img12 from '../../../images/portImages/img12.png'
+import img14 from '../../../images/portImages/img14.png'
 
 const portfolios=[
   {
@@ -60,6 +67,69 @@ const portfolios=[
     link2:'https://mrsuber.github.io/odin_project_08_LoginAndSignupFroms/',
     title:'Forms Display',
     text:'Odin excercise Number 08.Html css and js'
+  },
+  {
+    id:7,
+    category:'HTML-CSS-JS',
+    image:img7,
+    link1:'https://github.com/mrsuber/odin_project_07_todo_app',
+    link2:'https://mrsuber.github.io/odin_project_07_todo_app/',
+    title:'Todo App',
+    text:'Odin excercise Number 07.Html css and js'
+  },
+  {
+    id:8,
+    category:'HTML-CSS-JS',
+    image:img8,
+    link1:'https://github.com/mrsuber/odin_project_06_restaurant',
+    link2:'https://mrsuber.github.io/odin_project_06_restaurant/',
+    title:'Resturanct web',
+    text:'Odin excercise Number 06.Html css and js'
+  },
+  {
+    id:9,
+    category:'HTML-CSS-JS',
+    image:img9,
+    link1:'https://github.com/mrsuber/odin_project_09_api_calls',
+    link2:'https://mrsuber.github.io/odin_project_09_api_calls/',
+    title:'Find meals for your ingrident web',
+    text:'Odin excercise Number 09.Html css and js.matering api calls to external servers and managing promises'
+  },
+  {
+    id:10,
+    category:'HTML-CSS-JS',
+    image:img10,
+    link1:'https://github.com/mrsuber/odin_project_10_displaying_weater_using_api_calls',
+    link2:'https://mrsuber.github.io/odin_project_10_displaying_weater_using_api_calls/',
+    title:'weather forecast',
+    text:'Odin excercise Number 10.Html css and js.matering ASYNC and AWAIT funtions'
+  },
+  {
+    id:11,
+    category:'HTML-CSS-JS',
+    image:img11,
+    link1:'https://github.com/mrsuber/odin_project_12_BattleShip_Game',
+    link2:'https://mrsuber.github.io/odin_project_12_BattleShip_Game/',
+    title:'Battle Ship Game',
+    text:'Odin excercise Number 12.Html css and js.'
+  },
+  {
+    id:12,
+    category:'React',
+    image:img12,
+    link1:'https://github.com/mrsuber/odin_project_13_Introduction_to_React/tree/master/task-app',
+    link2:'https://modest-goldwasser-fe077f.netlify.app/',
+    title:'Introduction to React',
+    text:'Odin excercise Number 13.Task traker'
+  },
+  {
+    id:13,
+    category:'React',
+    image:img14,
+    link1:'https://github.com/mrsuber/odin_project_14_cvBuilder',
+    link2:'https://my-portfolio-fc70b.web.app/cvbuilder',
+    title:'CV Builder',
+    text:'Odin excercise Number 14.'
   },
 ]
 export default portfolios
