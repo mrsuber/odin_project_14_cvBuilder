90638e8e50ef865938868df5293a7a02b07b956f
mohamad <msiysinyuy@gmail.com>
2021-08-15 18:52:09 +0100

    the porfolio updated

--- cv-builder/src/components/MyProtfolioComponents/data/protfolios.js
+++ cv-builder/src/components/MyProtfolioComponents/data/protfolios.js
@@ -1,54 +1,65 @@
 import img1 from '../../../images/portImages/img1.png'
-import img2 from '../../../images/portImages/maya-4.jpg'
-import img3 from '../../../images/portImages/maya-3.jpg'
-// import img4 from '../../../images/portImages/maya-1.jpg'
+import img2 from '../../../images/portImages/img2.png'
+import img3 from '../../../images/portImages/img3.png'
+import img4 from '../../../images/portImages/img4.png'
+import img5 from '../../../images/portImages/img5.png'
+import img6 from '../../../images/portImages/img6.png'
 
 const portfolios=[
   {
     id:1,
     category:'React',
     image:img1,
-    link1:'#',
-    link2:'#',
-    title:'How to Use Python',
-    text:'Number one Programming Language'
+    link1:'https://github.com/mrsuber/odin_project_14_cvBuilder',
+    link2:'https://my-portfolio-fc70b.web.app',
+    title:'My Portfolio Web',
+    text:'This is the site that shows all i have archived so far'
   },
   {
     id:2,
-    category:'Animation',
+    category:'React',
     image:img2,
-    link1:'#',
-    link2:'#',
-    title:'Animate In Maya',
-    text:'Number one Animation Application'
+    link1:'https://github.com/mrsuber/Netflix-clone',
+    link2:'https://netflix-clone-8365e.web.app/',
+    title:'Netflix Clones',
+    text:'This aplication a netflix clone that play trillars when clicked'
   },
   {
     id:3,
-    category:'Python',
+    category:'React',
     image:img3,
-    link1:'#',
-    link2:'#',
-    title:'How To Use Blender',
-    text:'Free Animation Software'
+    link1:'https://github.com/mrsuber/Amazon-clone-React',
+    link2:'https://e-chalange2.web.app/',
+    title:'Amazone Clone',
+    text:'fullstack, firebase authentication and react front end stripe creadit card payment'
   }
   ,
   {
     id:4,
-    category:'Animation',
-    image:img2,
-    link1:'#',
-    link2:'#',
-    title:'How To Use Blender',
-    text:'Free Animation Software'
+    category:'HTML-CSS-JS',
+    image:img4,
+    link1:'https://github.com/mrsuber/odin_project_02',
+    link2:'https://mrsuber.github.io/odin_project_02/',
+    title:'Scketch Bourd',
+    text:'Odin excercise Number 02. Html,css and JS'
   },
   {
     id:5,
-    category:'JavaScript',
-    image:img2,
-    link1:'#',
-    link2:'#',
-    title:'Animate In Maya',
-    text:'Number one Animation Application'
-  }
+    category:'HTML-CSS',
+    image:img5,
+    link1:'https://github.com/mrsuber/google-homepage',
+    link2:'https://mrsuber.github.io/google-homepage/',
+    title:'Google Search Page Clone',
+    text:'Odin excercise Number 01.only html and css'
+  },
+  {
+    id:6,
+    category:'HTML-CSS-JS',
+    image:img6,
+    link1:'https://github.com/mrsuber/odin_project_08_LoginAndSignupFroms',
+    link2:'https://mrsuber.github.io/odin_project_08_LoginAndSignupFroms/',
+    title:'Forms Display',
+    text:'Odin excercise Number 08.Html css and js'
+  },
 ]
 export default portfolios
